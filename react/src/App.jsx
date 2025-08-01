edc1cfc9a9722f0e052897206c43ad418812fbdd
You Song Hou <ysongweb3@gmail.com>
2025-07-31 20:24:40 -0400

    chore: install react-router-dom

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,11 +1,21 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 import TokenBalanceDashboard from "./pages/TokenBalanceDashboard";
 import NFTDashboard from "./pages/NFTDashboard";
+
 function App() {
 
   return (
-    <>
-      <NFTDashboard />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/nft"
+         element={<NFTDashboard />} />
+        <Route
+          path="/"
+          element={<TokenBalanceDashboard />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
